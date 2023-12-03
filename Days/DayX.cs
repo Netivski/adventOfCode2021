0824794da30d71e0dd6688801afe753387ab8a60
Netivski <neto.lx@gmail.com>
2023-12-03 16:53:27 +0000

    Submiting local changes

---
+++ Days/DayX.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+
+    class Day14 {
+
+        public static readonly string App = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        public static readonly string Inputs = Path.Combine(App, "Inputs");
+
+
+        public static void First() {
+            var lines = Utils.ReadLines(Path.Combine(Inputs, "Day14.txt")).ToArray();
+        }
+
+        
+
+        public static void Second() {
+
+            var lines = Utils.ReadLines(Path.Combine(Inputs, "Day14.txt")).ToArray();
+            
+        }
+    }
+}
